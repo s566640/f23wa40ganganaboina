2b1812015d552c121b0ffc76877b3e1dbd19b1c4
s566640 <s566640@nwmissouri.edu>
2023-10-24 22:09:58 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Rajya Lakshmi Ganganaboina' });
+  res.render('mydata', { title: 'Rajya Lakshmi Ganganaboina'});
 });
 
 module.exports = router;
